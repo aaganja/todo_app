4e06a517dc340e221eabeea0ebffa68ec51afd70
sangam <snngm.sth@gmail.com>
2023-03-30 12:46:03 +0545

    create task and render task form in index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,19 @@ class TasksController < ApplicationController
     @tasks = @project.tasks
   end
 
+  def show
+  end
+
   def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to project_tasks_path, notice: "Task was successfully created." }
+      else
+        format.html { redirect_to project_tasks_path, alert: @task.errors.full_messages.join(',') }
+      end
+    end
   end
 
   def update
@@ -20,5 +32,13 @@ class TasksController < ApplicationController
   def set_project
     @projects = current_user.projects
     @project = current_user.projects.find(params[:project_id])
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :project_id, :due_date, :completed_at, :user_id)
   end
 end
