7553c867c628615b21efbe9b59993c6e430f7ba1
kuber aaganja <kuberaaganja@gmail.com>
2023-03-30 16:25:24 +0545

    Add admin view for admin user

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,15 @@
+class AdminController < ApplicationController
+  before_action :authenticate_user!
+
+  before_action :authorize_admin
+
+  def index
+    @tasks = Task.all
+  end
+
+  private
+  def authorize_admin
+    flash[:alert] = "You are authorized to visit this page"
+    redirect_to root_path unless current_user.admin?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'admin', to: 'admin#index'
   resources :projects do
     resources :tasks, :only => [:index, :create, :update, :new]
     post 'tasks/complete', to: 'tasks#complete'
     get 'task_completed', to: 'tasks#completed'
   end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
