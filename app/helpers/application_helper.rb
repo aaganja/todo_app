ee440b078f136c9764b91274e2bb0552900cf602
kuber aaganja <kuberaaganja@gmail.com>
2023-04-01 12:30:21 +0545

    fix the multiple task not being savaed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association,
       new_object,
-      :child_index => "new_#{association}") do |builder|
+      :child_index => "new#{association}") do |builder|
         render(association.to_s.singularize + "_fields", f: builder)
       end
 
