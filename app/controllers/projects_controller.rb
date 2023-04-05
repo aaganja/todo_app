fd9de03a77a6f7ea50bff9b430473a5aee4e4627
kuber aaganja <kuberaaganja@gmail.com>
2023-04-05 21:22:31 +0545

    change method name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,7 +19,7 @@ class ProjectsController < ApplicationController
     @project = @projects.new(project_params)
 
     respond_to do |format|
-      if !validate_due_date
+      if invalid_due_date?
         flash[:alert] = 'Task due date can not be more than three on same date.'
         format.html { render :new }
         format.json { render json: @project.errors, status: :unprocessable_entity }
@@ -48,13 +48,13 @@ class ProjectsController < ApplicationController
         end
     end
 
-    def validate_due_date
+    def invalid_due_date?
       due_date_counts = @project.tasks.group_by(&:due_date).map { |key, items| [key, items.count] }
-      valid_due_date = true
+      invalid_due_date = false
       due_date_counts.each do |a|
         return unless a[1].present?
-        valid_due_date = false if Task.where(due_date: a[0]).count + a[1] >= 3
+        invalid_due_date = true if Task.where(due_date: a[0]).count + a[1] >= 3
       end
-      valid_due_date
+      invalid_due_date
     end
 end
