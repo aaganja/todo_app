fc7804dd2894e25df8a3f588abed294f1a2b0c7a
kuber aaganja <kuberaaganja@gmail.com>
2023-04-06 14:46:56 +0545

    reindentent

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,11 +9,11 @@ class Project < ApplicationRecord
 
   def task_due_date
     due_date_counts = tasks.group_by(&:due_date).map { |key, items| [key, items.count] }
-      due_date_counts.each do |a|
-        return unless a[1].present?
-        if Task.where(due_date: a[0]).count + a[1] >= 3
-          return errors.add :due_date, "can not be more than three on same date."
-        end
+    due_date_counts.each do |a|
+      return unless a[1].present?
+      if Task.where(due_date: a[0]).count + a[1] >= 3
+        return errors.add :due_date, "can not be more than three on same date."
       end
+    end
   end
 end
